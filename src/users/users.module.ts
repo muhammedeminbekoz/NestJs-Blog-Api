dad114df39f110180f668e3593b9714e6936f736
Emin Beköz <m.eminbekoz19@gmail.com>
2024-07-21 17:56:33 +0300

    chore: Export UsersService from UsersModule

--- src/users/users.module.ts
+++ src/users/users.module.ts
@@ -7,5 +7,6 @@ import { PrismaModule } from 'src/prisma.module';
   imports: [PrismaModule],
   controllers: [UsersController],
   providers: [UsersService],
+  exports: [UsersService],
 })
 export class UsersModule {}
