8a76158ed1acf7567f385814167d599b1131620c
Emin Beköz <m.eminbekoz19@gmail.com>
2024-07-27 23:49:44 +0300

    chore: Update JWT token expiration to 30 seconds

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -10,7 +10,7 @@ import { JwtModule } from '@nestjs/jwt';
     JwtModule.register({
       global: true,
       secret: process.env.JWT_SERCRET_KEY,
-      signOptions: { expiresIn: '1d' },
+      signOptions: { expiresIn: '30s' },
     }),
   ],
   controllers: [AuthController],
