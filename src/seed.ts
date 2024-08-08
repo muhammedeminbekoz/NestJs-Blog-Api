189a4c0d6df4c2a1c121c9b6fca5d172dacba5d0
Emin Beköz <m.eminbekoz19@gmail.com>
2024-08-08 11:41:45 +0300

    refactor: Updated seed.ts to add related data

--- src/seed.ts
+++ src/seed.ts
@@ -4,8 +4,8 @@ import * as bcrypt from 'bcrypt';
 
 const prisma = new PrismaClient();
 
-export function seedMockData() {
-  Array.from({ length: 10 }).map(async () => {
+export function seedMockUserData() {
+  Array.from({ length: 100 }).map(async () => {
     await prisma.users.create({
       data: {
         firstname: faker.company.name(),
@@ -17,7 +17,27 @@ export function seedMockData() {
   });
 }
 
-/*
- */
+export async function seedMockPostRelationData() {
+  const users = await prisma.users.findMany();
 
-seedMockData();
+  if (users.length === 0) {
+    throw new Error(
+      'Kullanıcı yok, lütfen önce kullanıcı verilerini oluşturun.',
+    );
+  }
+  console.log(users);
+  await Promise.all(
+    Array.from({ length: 5000 }).map(async () => {
+      // Rastgele bir kullanıcı seç
+      const randomUser = users[Math.floor(Math.random() * users.length)];
+
+      await prisma.posts.create({
+        data: {
+          content: faker.lorem.paragraph({ min: 2, max: 50 }),
+          title: faker.lorem.sentence({ min: 1, max: 7 }),
+          userId: randomUser.id,
+        },
+      });
+    }),
+  );
+}
