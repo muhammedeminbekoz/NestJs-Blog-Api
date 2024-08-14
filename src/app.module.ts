2311bc951e60d41e91d5fbc356844190bcac66e0
Emin Beköz <m.eminbekoz19@gmail.com>
2024-08-14 19:02:59 +0300

    feat: Add PostsModule to AppModule imports

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,8 +1,9 @@
 import { Module } from '@nestjs/common';
 import { UsersModule } from './users/users.module';
 import { AuthModule } from './auth/auth.module';
+import { PostsModule } from './posts/posts.module';
 
 @Module({
-  imports: [UsersModule, AuthModule],
+  imports: [UsersModule, AuthModule, PostsModule],
 })
 export class AppModule {}
